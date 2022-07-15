4bbec363ea4b2093496b91950b56c603fbc2cb2d
Sunny (SunJae) Lee 이선재 <snjlee58@gmail.com>
2022-07-15 21:46:46 +0900

    Create CharacterCounter.java

---
+++ HW1/problem4/Skeleton/CharacterCounter.java
@@ -0,0 +1,13 @@
+public class CharacterCounter {
+    public static void countCharacter(String str) {
+        // DO NOT change the skeleton code.
+        // You can add codes anywhere you want.
+
+
+    }
+
+    private static void printCount(char character, int count){
+        System.out.printf("%c: %d times", character, count);
+    }
+
+}
