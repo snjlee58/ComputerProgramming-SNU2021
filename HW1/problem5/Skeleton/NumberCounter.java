7bae039de47fb308437fd2d7378a380252c36063
Sunny (SunJae) Lee 이선재 <snjlee58@gmail.com>
2022-07-15 21:48:09 +0900

    Create NumberCounter.java

---
+++ HW1/problem5/Skeleton/NumberCounter.java
@@ -0,0 +1,13 @@
+public class NumberCounter {
+    public static void countNumbers(String str0, String str1, String str2) {
+        // DO NOT change the skeleton code.
+        // You can add codes anywhere you want.
+        
+
+    }
+
+    private static void printNumberCount(int number, int count) {
+        System.out.printf("%d: %d times\n", number, count);
+    }
+
+}
