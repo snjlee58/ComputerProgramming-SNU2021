576f4faf231ab5e28869980a2a9c1fc699de1b0b
Sunny (SunJae) Lee 이선재 <snjlee58@gmail.com>
2022-07-15 21:49:19 +0900

    Create PrimeNumbers.java

---
+++ HW1/problem6/Skeleton/PrimeNumbers.java
@@ -0,0 +1,7 @@
+public class PrimeNumbers {
+    public static void printPrimeNumbers(int m, int n) {
+        // DO NOT change the skeleton code.
+        // You can add codes anywhere you want.
+
+    }
+}
