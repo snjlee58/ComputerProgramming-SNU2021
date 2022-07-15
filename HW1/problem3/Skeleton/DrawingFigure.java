5bcb08b60ad0b9cf161632c90d57a2c642f1bb21
Sunny (SunJae) Lee 이선재 <snjlee58@gmail.com>
2022-07-15 21:45:42 +0900

    Create DrawingFigure.java

---
+++ HW1/problem3/Skeleton/DrawingFigure.java
@@ -0,0 +1,9 @@
+public class DrawingFigure {
+    public static void drawFigure(int n) {
+        // DO NOT change the skeleton code.
+        // You can add codes anywhere you want.
+
+
+
+    }
+}
